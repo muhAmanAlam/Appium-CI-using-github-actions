cc700eff2fc479f690e9b83975ea62507a6a6a36
Izhanalikhan <61951371+Izhanalikhan@users.noreply.github.com>
2024-06-12 16:48:58 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,42 @@
+import unittest
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+# Import Appium UiAutomator2 driver for Android platforms (AppiumOptions)
+from appium.options.android import UiAutomator2Options
+
+capabilities = dict(
+    platformName='android',
+    appPackage='com.android.settings',
+    skipServerInstallation='true',
+    uiautomator2ServerLaunchTimeout='200000',
+    appActivity='.Settings',
+    language='en',
+    locale='US'
+)
+
+appium_server_url = 'http://localhost:4723'
+
+# Converts capabilities to AppiumOptions instance
+capabilities_options = UiAutomator2Options().load_capabilities(capabilities)
+
+class TestAppium(unittest.TestCase):
+    def setUp(self) -> None:
+        self.driver = webdriver.Remote(command_executor=appium_server_url,options=capabilities_options)
+
+    def tearDown(self) -> None:
+        if self.driver:
+            self.driver.quit()
+
+    def test_find_battery(self) -> None:
+        el = self.driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Battery"]')
+        el.click()
+        self.driver.get_screenshot_as_file('Screenshots\\battery.png')
+
+    def test_find_network(self) -> None:
+        el = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@resource-id="android:id/title" and @text="Network & internet"]')
+        el.click()
+        self.driver.get_screenshot_as_file('Screenshots\\network.png')
+
+if __name__ == '__main__':
+    unittest.main()
